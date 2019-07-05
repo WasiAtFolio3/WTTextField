617a5bcfedac84e54e22f5644c39a1a0b206b89e
Hamza Azad <hazad@folio3.com>
2019-07-05 11:13:00 +0500

    Initial Commit

---
+++ Custom TextfieldTests/Custom_TextfieldTests.swift
@@ -0,0 +1,34 @@
+//
+//  Custom_TextfieldTests.swift
+//  Custom TextfieldTests
+//
+//  Created by Wasi Tariq on 7/5/19.
+//  Copyright © 2019 Folio3. All rights reserved.
+//
+
+import XCTest
+@testable import Custom_Textfield
+
+class Custom_TextfieldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
